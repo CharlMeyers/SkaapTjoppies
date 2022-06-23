190d30ab623b4e072fb9724f4764964e67834aee
Charl <meyers.charl@gmail.com>
2022-06-23 17:10:48 +0200

    Add stuff

--- Company Cup/models/World.cs
+++ Company Cup/models/World.cs
@@ -7,10 +7,19 @@ namespace CompanyCup.models
     public class World
     {
         public List<Resource> resources { get; set; }
+        public Quota quota { get; set; }
+        public int quotaMultiplier { get; set; }
+        public Dimension size { get; set; }
+        public List<WorldLandscape> Lanscape { get; set; }
 
         public World()
         {
             resources = new List<Resource>();
+        }
+
+        public void AddLandscape(string landscapeDetails)
+        {
+
         }
     }
 }
